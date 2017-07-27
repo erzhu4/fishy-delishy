469630aae528a2ea871522baee8794f2ad30ca6a
eric.zhu <erzhu4@gmail.com>
2017-07-27 10:35:59 -0400

    add clockwork

---
+++ resources/views/register.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<title>Register</title>
+<head>
+    <link rel = "stylesheet"
+          type = "text/css"
+          href = "css/main.css" />
+    <script src="js/main.js"></script>
+</head>
+<body>
+    <h1>Register</h1>
+    <form action="/user/create" method="get">
+        <input name="email" type="text">
+        <input name="password" type="password">
+        <input name="password_confirm" type="password">
+    </form>
+</body>
+</html>
